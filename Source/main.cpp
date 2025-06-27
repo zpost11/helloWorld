381a7929a87c8dfb58cf58d972d1df25ad75a1c7
John Doe <johndoe@example.com>
2025-06-27 17:30:14 -0600

    Created main files

---
+++ Source/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void printHelloWorld();
+
+int main()
+{
+    printHelloWorld();
+    return 0;
+}
+
+void printHelloWorld()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
